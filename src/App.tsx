94838e0e65d38a97b1bb1a842191324f251e98ef
cherie <cherie@xiaoruis-mbp.lan>
2020-12-16 02:05:46 -0800

    :speech_balloon: clean the code style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
-}
+export const App = () =>
+(
+  <div className="App">
+    Hello World!
+  </div>
+);
 
-export default App;
